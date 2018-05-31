7b1d36e9b527862af6e26d67c12f7db61ee1d610
twestura <twestura@gmail.com>
2018-05-31 14:26:10 -0400

    Use the correct zenburn theme.

--- atompackages.sh
+++ atompackages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apm install zenburn-theme atom-beautify atom-clock atom-ide-ui build docblockr file-icons goto-definition language-latex language-markdown latex linter linter-pylint linter-ui-default markdown-scroll-sync markdown-writer platformio-ide-terminal script sort-lines todo-show vim-mode-plus
+apm install zenburn atom-beautify atom-clock atom-ide-ui build docblockr file-icons goto-definition language-latex language-markdown latex linter linter-pylint linter-ui-default markdown-scroll-sync markdown-writer platformio-ide-terminal script sort-lines todo-show vim-mode-plus
